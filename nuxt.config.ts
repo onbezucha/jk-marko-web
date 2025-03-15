f6ac13f827a72d7e5209509c1f3f573c59e230c5
Ondřej Bezucha <onbezucha@gmail.com>
2025-03-15 17:57:20 +0100

    Hotfix
    Hotfix Posthog

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+  modules: ['nuxt-gtag'],
 
   vite: {
     plugins: [
@@ -12,15 +13,15 @@ export default defineNuxtConfig({
     ],
   },
 
-  modules: ['nuxt-gtag', 'nuxt-posthog'],
   gtag: {
     id: 'G-KXZTCZSH'
   },
 
   runtimeConfig: {
     public: {
-      posthogPublicKey: 'phc_50YQ26jgQsHhB0aXWkZyZ0HTawNM73nlbkyx0iwTVnx',
-      posthogHost: 'https://us.i.posthog.com'
-    }
-  }
+      posthogPublicKey: "phc_3QRZsJvUxYolNaspDmh1Zpq9cjSG4TG6Pvc0HFCjtH9",
+      posthogHost: "https://eu.i.posthog.com"
+     }
+   }
+
 })
