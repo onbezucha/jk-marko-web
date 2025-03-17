f8578dabb950f3247f29ff3f1ff05d5de104db41
Ondřej Bezucha <onbezucha@gmail.com>
2025-03-17 21:25:48 +0100

    Beta 1.1.1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,8 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-import tailwindcss from "@tailwindcss/vite";
-
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
-  modules: ["nuxt-gtag", "@nuxt/icon", "@nuxt/content"],
-
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  modules: ["nuxt-gtag", "@nuxt/icon", "@nuxt/content", "@nuxtjs/tailwindcss"],
 
   gtag: {
     id: "G-KXZTCZSH",
@@ -19,21 +12,6 @@ export default defineNuxtConfig({
     public: {
       posthogPublicKey: "phc_3QRZsJvUxYolNaspDmh1Zpq9cjSG4TG6Pvc0HFCjtH9",
       posthogHost: "https://eu.i.posthog.com",
-    },
-  },
-
-  content: {
-    // Základní konfigurace content modulu
-    documentDriven: true,
-    navigation: {
-      fields: ["title", "description"],
-    },
-    // Pokud používáte Markdown s frontmatter, můžete zde nastavit další parametry
-    markdown: {
-      toc: {
-        depth: 3,
-        searchDepth: 3,
-      },
     },
   },
 });
